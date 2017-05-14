6f42590856d37ae56de1e52a3436e111abdd8441
trkone <tar_x1@hotmail.com>
2017-05-14 10:01:09 +0300

    Add files via upload

---
+++ Define_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#define PI 3.14  //	"const double PI=3.14" şeklindede yazılabilir sabit bir ifadedir.
+
+#define PI2() 3.14 // "PI2(){return 3.14;}" şeklindede yazılabilir ınline bir fonksiyondur.
+
+#define Kare(A) A*A // template ilede yazılabilir onun orneğini daha sonra gostericem
+
+
+int main()
+{
+	cout<<"Sabit:"<<PI<<endl;
+	cout<<"Inline:"<<PI2()<<endl<<endl
+	
+	<<"Kare(double):"<<Kare(2.1)<<endl<<"Kare(int):"<<Kare(3)<<endl;
+	
+	return 0;
+}
