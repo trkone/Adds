09c305714db9f1dcf7fc3b45399907d82ac3ce88
trkone <tar_x1@hotmail.com>
2017-05-14 13:12:05 +0300

    Update Define_1.cpp

--- Define_1.cpp
+++ Define_1.cpp
@@ -4,7 +4,7 @@ using std::endl;
 
 #define PI 3.14  //	"const double PI=3.14" şeklindede yazılabilir sabit bir ifadedir.
 
-#define PI2() 3.14 // "PI2(){return 3.14;}" şeklindede yazılabilir ınline bir fonksiyondur.
+#define PI2() 3.14 // "inline double PI2(){return 3.14;}" şeklindede yazılabilir ınline bir fonksiyondur.
 
 #define Kare(A) A*A // template ilede yazılabilir onun orneğini daha sonra gostericem
 
